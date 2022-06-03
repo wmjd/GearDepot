92f921da8b37f822dac007ea2cd70ddfcbd1479d
William Diebolt <wmdiebolt@gmail.com>
2022-06-03 12:50:15 -0700

    update sql data definition

--- db.sql
+++ db.sql
@@ -3,31 +3,32 @@ CREATE DATABASE gearTracker;
 USE gearTracker;
 
 CREATE TABLE users (
-    user_id VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
+	user_id VARCHAR(255) NOT NULL,
+	first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE gear (
-gear_id INT NOT NULL AUTO_INCREMENT,
-    category VARCHAR(255),
+	gear_id INT NOT NULL AUTO_INCREMENT,
+    category VARCHAR(255) NOT NULL,
     manufacturer VARCHAR(255),
-    product_name VARCHAR(255),
+	product_name VARCHAR(255) NOT NULL,
     color VARCHAR(255),
-    quantity VARCHAR(255),
+    available INT NOT NULL,
+    quantity INT NOT NULL,
     notes VARCHAR(255),
     PRIMARY KEY (gear_id)
 );
 
 CREATE TABLE tickets (
-    ticket_id INT NOT NULL,
+	ticket_id INT NOT NULL AUTO_INCREMENT,
     gear_id INT NOT NULL,
     user_id VARCHAR(255) NOT NULL,
     quantity INT NOT NULL,
-    date_out VARCHAR(255) NOT NULL,
-    date_in VARCHAR(255),
+    date_out DATE NOT NULL,
+    date_in DATE,
     destination VARCHAR(255) NOT NULL,
     FOREIGN KEY (gear_id) REFERENCES gear(gear_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
